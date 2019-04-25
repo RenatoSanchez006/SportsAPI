9f41d9740a50dde447cfbde4773ab4be934ff89a
Renato Sánchez <renato@emissary.mx>
2019-04-25 08:00:18 -0500

    .

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL //|| 'mongodb+srv://rsanchez:rsanchez@renatocluster-5frwb.mongodb.net/sports?retryWrites=true';
- || 'mongodb://localhost/sports';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/sports';
 exports.PORT = process.env.PORT || 8080;
+
+// mongodb+srv://rsanchez:<password>@renatocluster-5frwb.mongodb.net/test?retryWrites=true
